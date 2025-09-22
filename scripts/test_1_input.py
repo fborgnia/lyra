713f9b23637e88d823f9f9b9771357ad995e2833
Fede Borgnia <fborgnia@gmail.com>
2025-09-22 18:21:18 +0100

    Add a couple of test scripts to test 1 and 2 turn executions

---
+++ scripts/test_1_input.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import torch
+
+# Add the project root to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from lyra.model import GemmaWithMemory
+
+model = GemmaWithMemory()
+
+# --- Turn 1: Create the memory ---
+# This turn's information will be stored in the memory graph, but NOT passed in the next prompt.
+print("--- Turn 1: Storing memory ---")
+prompt1 = "<start_of_turn>user\nMy Keyring is blue.<end_of_turn>\n<start_of_turn>model\n"
+inputs1 = model.tokenizer(prompt1, return_tensors="pt")
+
+outputs1 = model.generate(
+    input_ids=inputs1["input_ids"],
+    attention_mask=inputs1["attention_mask"],
+    max_new_tokens=150,
+    eos_token_id=model.end_of_turn_token_id
+)
+full_text1 = model.tokenizer.decode(outputs1[0], skip_special_tokens=False)
+print(f"\n--- Model Output (Turn 1) ---\n{full_text1}\n--------------------\n")
